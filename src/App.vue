599a4aab54436b16ce133c9ce469cadb4eed5fb0
Greg Hovanesyan <greg.hovanesyan@gmail.com>
2019-02-22 12:55:16 +0400

    Init

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <funnel-graph></funnel-graph>
+  </div>
+</template>
+
+<script>
+import FunnelGraph from './components/FunnelGraph.vue';
+
+export default {
+  name: 'app',
+  components: {
+    FunnelGraph,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/FunnelGraph.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="hello">
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'FunnelGraph',
+  props: {
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
